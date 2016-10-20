4285d725c17bd5cd807296726c9a9660256b8403
Fuyuan Xue <xfycn@vip.qq.com>
2016-10-20 22:25:32 +0800

    reGit

--- lab1.java
+++ lab1.java
@@ -3,7 +3,7 @@ package lab1;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-//git test line
+//git test
 public class lab1 {
 	public static Scanner s = new Scanner(System.in);// 全局变量
 	public static String str = null;
