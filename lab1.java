d352ffaf7f2b81079bb0ac707aeae80ccf617024
Wxf <1522745081@qq.com>
2016-10-24 21:50:55 +0800

    Wxf VisualVM change lab1.java

--- lab1.java
+++ lab1.java
@@ -33,6 +33,54 @@ public class Lab1 {
   * data manager(返回值说明)
   * @throws Exception if has error(异常说明)
   */
+  public static void derivative(String exp, char dx) {
+    // 函数参数：exp为要处理的表达式，dx为要求导的变量； 返回值： 无
+    String[] exparray0 = exp.split("\\+");// 对等式已"+"进行分割，各项储存在exp_array字符串数组中
+    int explength0 = exparray0.length;// 计算数组项目数
+    for (int i = 0; i < explength0; i++) {
+
+      int num = 0;// 要求导的变量出现的次数
+      char[] charcount0 = exparray0[i].toCharArray();
+      //计数
+      for (int j = 0; j < charcount0.length; j++) {
+        if (charcount0[j] == dx) {
+          num++;
+        }
+      }
+      // 在变量出现为0次的式子里，导数值为0
+      if (num == 0) {
+        exparray0[i] = "0";// 将导数值替换掉原值
+      } else {
+        String[] temparray0 = exparray0[i].split("");// 分割
+        String tempstring = "";
+        // 遍历，替换第一个
+        for (int k = 0; k < temparray0.length; k++) {
+          if (temparray0[k].equals(String.valueOf(dx))) {
+            temparray0[k] = String.valueOf(num);
+            break;
+          }
+
+        }
+        for (int o = 0; o < temparray0.length; o++) {
+          tempstring += temparray0[o];
+        }
+        exparray0[i] = tempstring;// 将导数值替换掉原值
+      }
+    }
+    // 输出结果
+    for (int z = 0; z < exparray0.length - 1; z++) {
+      System.out.print(exparray0[z]);
+      System.out.print("+");
+    }
+    System.out.print(exparray0[exparray0.length - 1]);
+  }
+  
+  /**
+  * set default mock parameter.（方法说明）
+  * additionalParameters parameter additional(参数名称)
+  * data manager(返回值说明)
+  * @throws Exception if has error(异常说明)
+  */
   public static void simplify() {
     // 简化
     System.out.println("Please input your order:");
@@ -94,52 +142,4 @@ public class Lab1 {
     }
   }
   
-  /**
-  * set default mock parameter.（方法说明）
-  * additionalParameters parameter additional(参数名称)
-  * data manager(返回值说明)
-  * @throws Exception if has error(异常说明)
-  */
-  public static void derivative(String exp, char dx) {
-    // 函数参数：exp为要处理的表达式，dx为要求导的变量； 返回值： 无
-    String[] exparray0 = exp.split("\\+");// 对等式已"+"进行分割，各项储存在exp_array字符串数组中
-    int explength0 = exparray0.length;// 计算数组项目数
-    for (int i = 0; i < explength0; i++) {
-
-      int num = 0;// 要求导的变量出现的次数
-      char[] charcount0 = exparray0[i].toCharArray();
-      //计数
-      for (int j = 0; j < charcount0.length; j++) {
-        if (charcount0[j] == dx) {
-          num++;
-        }
-      }
-      // 在变量出现为0次的式子里，导数值为0
-      if (num == 0) {
-        exparray0[i] = "0";// 将导数值替换掉原值
-      } else {
-        String[] temparray0 = exparray0[i].split("");// 分割
-        String tempstring = "";
-        // 遍历，替换第一个
-        for (int k = 0; k < temparray0.length; k++) {
-          if (temparray0[k].equals(String.valueOf(dx))) {
-            temparray0[k] = String.valueOf(num);
-            break;
-          }
-
-        }
-        for (int o = 0; o < temparray0.length; o++) {
-          tempstring += temparray0[o];
-        }
-        exparray0[i] = tempstring;// 将导数值替换掉原值
-      }
-    }
-    // 输出结果
-    for (int z = 0; z < exparray0.length - 1; z++) {
-      System.out.print(exparray0[z]);
-      System.out.print("+");
-    }
-    System.out.print(exparray0[exparray0.length - 1]);
-  }
-
 }
